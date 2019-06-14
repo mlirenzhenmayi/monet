405151d4f02172e7413a2f4ca5256bcfbacc943c
amcz <alicec@umd.edu>
2019-06-14 14:31:15 -0400

    cems_api.key added warning if apikey is not found. metfiles.py fixed bug in finding self.dmt attribute

--- monet/utilhysplit/metfiles.py
+++ monet/utilhysplit/metfiles.py
@@ -45,8 +45,8 @@ class MetFiles:
     def find_mdt(self):
         # finds time spacing between met files by
         # seeing which spacing produces a new file name.
-        testdate = datetime.datetime(2010,10,12)
-        mdtlist = [1,24,24*7,24*7*4, 24*356]
+        testdate = datetime.datetime(2010,1,1)
+        mdtlist = [1,24,24*7,24*31, 24*356]
         
         file1 = testdate.strftime(self.strfmt)
         done = False
@@ -55,7 +55,7 @@ class MetFiles:
             dt = datetime.timedelta(hours=mdtlist[iii])
             d2 = testdate   + dt
             file2 = d2.strftime(self.strfmt)
-            if d2 != testdate: done=True
+            if file2 != file1: done=True
             iii += 1
             if iii >= len(mdtlist): done=True
         return  dt
@@ -103,6 +103,7 @@ class MetFiles:
             else:
               if temp not in nlist:
                  nlist.append(temp)
+            #print(edate, '--' , end_date, '--' , self.mdt)
             if edate > end_date:
                 done = True
             if zzz > 50: done=True 
